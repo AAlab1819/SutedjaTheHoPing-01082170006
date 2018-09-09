8e491e812d4dd05f0ba02868231900df9cae5384
tedja3001 <42811738+tedja3001@users.noreply.github.com>
2018-09-09 09:16:20 +0700

    Add files via upload

---
+++ Week02-Sorting/768A - Oath Of The Night's Watch.h
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int shellSort(int arr[], int n)
+{
+    for (int gap = n/2; gap>0; gap/=2)
+        for (int i=gap; i<n; i++)
+            for(int j=i-gap; j>=0; j-=gap)
+            {
+                if (arr[j] > arr [j+gap])
+                    swap(arr[j], arr[j+gap]);
+            }
+
+}
+
+int main()
+{
+    int x;
+    cin>>x;
+    int arr[x];
+
+
+    int smallestAmount=0;
+    int largestAmount=0;
+    for(int i=0; i<x; i++)
+    {
+        int temp;
+        cin>>temp;
+    }
+    shellSort (arr, x);
+    for(int i=0; i<x; i++)
+    {
+        if(arr[i] == arr[0])
+        {
+            smallestAmount++;
+        }else{
+        break;
+        }
+    }
+    for(int i=x-1; i>=0; i--)
+    {
+        if (arr[i] == arr[x-1])
+        {
+            largestAmount++;
+        }
+        else{
+            break;
+        }
+    }
+    cout << x-(smallestAmount+largestAmount);
+    return 0;
+}
